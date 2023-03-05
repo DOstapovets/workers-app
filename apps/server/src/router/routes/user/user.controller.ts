ecebfd64dc101ba09af8fab28fa1b1e49b1434c9
Dmytro Ostapovets <ostapovets.d@gmail.com>
2023-03-05 15:37:46 +0100

    fix: user profile

--- apps/server/src/router/routes/user/user.controller.ts
+++ apps/server/src/router/routes/user/user.controller.ts
@@ -1,8 +1,9 @@
+import type { User } from 'app-types';
 import type { NextFunction, Request, Response } from 'express';
-
 import loggerFactory from 'app-logger';
 
 import userService from './user.service';
+import uploadsService from '../uploads/uploads.service';
 
 const log = loggerFactory('UserCtrl');
 
@@ -21,9 +22,63 @@ class UserController {
   async post(req: Request, res: Response, next: NextFunction) {
     log.debug('Create user');
     try {
-      const users = await userService.createUser(req.body);
+      const { files, body } = req;
+      const imageKeys = ['avatar', 'cover'] as ('avatar' | 'cover')[];
+      const { avatar, cover, ...params } = body;
 
-      res.json(users);
+      const user = await userService.createUser(params);
+
+      if (files && !Array.isArray(files) && Object.keys(files).length) {
+        log.debug('Upload files');
+
+        const uploadsPromises = imageKeys.map(async (key) => {
+          if (files[key]?.length) {
+            return uploadsService
+              .uploadNewFile(files[key][0], user.id)
+              .then((upload) => {
+                (user as User)[key] = upload;
+              });
+          }
+
+          return Promise.resolve();
+        });
+
+        await Promise.all(uploadsPromises);
+
+        await user.save();
+      } else if (avatar || cover) {
+        log.debug('Upload files from url');
+
+        const uploadsPromises = imageKeys.map(async (key) => {
+          if (body[key]) {
+            return uploadsService
+              .uploadFileFromUrl(body[key], user.id)
+              .then((upload) => {
+                (user as User)[key] = upload;
+              });
+          }
+        });
+
+        await Promise.all(uploadsPromises);
+
+        await user.save();
+      }
+
+      res.json(user);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async patchOne(req: Request, res: Response, next: NextFunction) {
+    log.debug('Update user');
+    try {
+      const updatedUser = await userService.updateUserById(
+        req.params.id,
+        req.body,
+      );
+
+      return res.json(updatedUser);
     } catch (err) {
       next(err);
     }
